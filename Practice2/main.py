00b167ed60d34a65f7e1d4aa38d5ebd3d06ea23f
Egor <161602848+beato07@users.noreply.github.com>
2025-09-14 19:00:47 +0300

    Implement load_clean_wine_data func

--- Practice2/main.py
+++ Practice2/main.py
@@ -2,12 +2,27 @@ import pandas as pd
 from sklearn.datasets import load_wine
 
 
-def import_load_wine_dataset():
-    data = load_wine()
-    df = pd.DataFrame(data.data, columns=data.feature_names)
-    print(df.info())
+data = load_wine(as_frame=True)
+df = pd.DataFrame(data.data, columns=data.feature_names)
+
+
+def load_wine_data():
+    print(df)
     return df
 
 
+def load_clean_wine_data():
+    if df.isna().any().any():
+        df_cleaned = df.dropna()
+        print(f"Rows deleted: {len(df_cleaned)}")
+    print(df.info())
+    print(df.info())
+
+
 if __name__ == '__main__':
-    res1 = import_load_wine_dataset()
+
+    # Выгрузка многомерных данных
+    res1 = load_wine_data()
+
+    # Вывод информации о данных, используя info(), info()
+    load_clean_wine_data()
