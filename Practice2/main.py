338af39f62ae34d36144feaf9de0ff7831c35361
Egor <161602848+beato07@users.noreply.github.com>
2025-09-14 21:26:33 +0300

    Implement draw_bar_chart func

--- Practice2/main.py
+++ Practice2/main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.datasets import load_wine
+import plotly.graph_objects as go
 
 
 data = load_wine(as_frame=True)
@@ -19,6 +20,32 @@ def load_clean_wine_data():
     print(df.info())
 
 
+def draw_bar_chart():
+    x_data = ['14.09.2015', '06.06.2017', '01.03.2020', '20.07.2023']
+    y_date = [1813.70, 4206.14, 2750.52, 749.99]
+
+    fig = go.Figure()
+    fig.add_trace(go.Bar(x=x_data, y=y_date,
+                         marker=dict(color=y_date,
+                                     coloraxis='coloraxis',
+                                     line=dict(color='black', width=2))))
+    fig.update_layout(
+        title="Diagram",
+        title_x=0.5,
+        title_font_size=20,
+        xaxis_title = 'Date', xaxis_title_font_size=14,
+        yaxis_title='Value', yaxis_title_font_size=14,
+        height=700)
+    fig.update_xaxes(
+        tickangle=315,
+        gridwidth=2,
+        gridcolor='ivory')
+    fig.update_yaxes(
+        gridwidth=2,
+        gridcolor='ivory')
+    fig.show()
+
+
 if __name__ == '__main__':
 
     # Выгрузка многомерных данных
@@ -26,3 +53,6 @@ if __name__ == '__main__':
 
     # Вывод информации о данных, используя info(), info()
     load_clean_wine_data()
+
+    # Построение столбчатой диаграммы
+    draw_bar_chart()
