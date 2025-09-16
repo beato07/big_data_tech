36a9ca99c24b8d33c4cc5cd3d50ee633d967b587
Egor <161602848+beato07@users.noreply.github.com>
2025-09-16 22:26:04 +0300

    Data frame op fix

--- Practice2/main.py
+++ Practice2/main.py
@@ -3,21 +3,11 @@ from sklearn.datasets import load_wine
 import plotly.graph_objects as go
 
 
-data = load_wine(as_frame=True)
-df = pd.DataFrame(data.data, columns=data.feature_names)
-
-
-def load_wine_data():
-    print(df)
-    return df
-
-
-def load_clean_wine_data():
+def load_clean_wine_data(df):
     if df.isna().any().any():
         df_cleaned = df.dropna()
         print(f"Rows deleted: {len(df_cleaned)}")
-    print(df.info())
-    print(df.info())
+    return df.info()
 
 
 def draw_bar_chart():
@@ -70,10 +60,12 @@ def draw_pie_chart():
 if __name__ == '__main__':
 
     # Выгрузка многомерных данных
-    res1 = load_wine_data()
+    data = load_wine(as_frame=True)
+    df = pd.DataFrame(data.data, columns=data.feature_names)
+    print(df)
 
     # Вывод информации о данных, используя info(), info()
-    load_clean_wine_data()
+    print(load_clean_wine_data(df))
 
     # Построение столбчатой диаграммы
     draw_bar_chart()
