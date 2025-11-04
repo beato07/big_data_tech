5dc7c7b9b0dfe47eb41beea2388849f9f1684d39
Egor <egor.pyuretskiy@gmail.com>
2025-11-04 17:10:19 +0300

    Implement task_2 func

--- Practice4/main.py
+++ Practice4/main.py
@@ -1,5 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 
 
 def task_1():
@@ -10,7 +13,7 @@ def task_1():
     street = np.array([80, 98, 75, 91, 78])
     garage = np.array([100, 82, 105, 89, 102])
 
-    print(f"Корреляция Пирсона между 'Улица' и 'Гараж': {np.corrcoef(street, garage)[0, 1]}")
+    print(f"\nКорреляция Пирсона между 'Улица' и 'Гараж': {np.corrcoef(street, garage)[0, 1]}")
 
     plt.scatter(street, garage, color='crimson')
     plt.title('Диаграмма рассеяния', fontsize=20)
@@ -21,5 +24,46 @@ def task_1():
     plt.show()
 
 
+def task_2():
+    """
+    Выводит данные, проводит предобработку и описывает признаки.
+    Строит корреляционную матрицу по одной целевой переменной.
+    Реализует и визуализирует регрессию на графике.
+    Отображает наклон, сдвиг и MSE.
+    """
+    sl = pd.read_csv('insurance.csv')
+    print(f"\nРазмерность данных: {sl.shape}")
+    print(f"\nКолонки: {list(sl.columns)}")
+    print(f"\nПроверка на пропущенные значения:\n{sl.isnull().sum()}")
+
+    df = pd.DataFrame(sl , columns=['age', 'bmi', 'children', 'charges'])
+    corr_matrix = df.corr().round(3)
+
+    print(f"\nМАТРИЦА КОРРЕЛЯЦИИ:\n{corr_matrix}")
+    print(f"\nНАИБОЛЕЕ КОРРЕЛИРУЮЩАЯ ПЕРЕМЕННАЯ С CHARGES: {corr_matrix['charges'].nlargest(2).index[-1]}")
+
+    model = LinearRegression()
+    x = df[['age']]
+    y = df['charges']
+    model.fit(x,y)
+
+    model_y_sk = model.coef_[0] * x + model.intercept_
+    fig = plt.figure(figsize=(10, 6))
+    plt.plot(x, model_y_sk, linewidth=2, color='r',
+             label=f'linear_model = {model.coef_[0]:.2f}x + {model.intercept_:.2f}')
+    plt.scatter(x, y, alpha=0.7)
+    plt.grid()
+    plt.xlabel('age')
+    plt.ylabel('charges')
+    plt.legend()
+    plt.show()
+
+    print(f"\n=== ПАРАМЕТРЫ РЕГРЕССИОННОЙ МОДЕЛИ ===")
+    print(f"    Угол наклона: {model.coef_[0]:.4f}")
+    print(f"    Коэффициент сдвига: {model.intercept_:.2f}")
+    print(f"    MSE: {mean_squared_error(model_y_sk, y):.2f}")
+
+
 if __name__ == '__main__':
     task_1()
+    task_2()
